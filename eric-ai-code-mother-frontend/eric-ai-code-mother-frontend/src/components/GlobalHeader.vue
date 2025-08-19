fa87d25c40c2c99e5bf134b59c586068b5012fca
eric <543510981@qq.com>
2025-08-19 23:25:28 +0800

    feat(user): 实现用户登录和注册功能
    - 新增用户登录和注册页面组件
    - 实现用户登录、注册、退出登录等接口
    - 添加全局登录状态管理
    - 更新路由配置，支持用户相关页面

--- eric-ai-code-mother-frontend/eric-ai-code-mother-frontend/src/components/GlobalHeader.vue
+++ eric-ai-code-mother-frontend/eric-ai-code-mother-frontend/src/components/GlobalHeader.vue
@@ -22,7 +22,25 @@
       <!-- 右侧：用户操作区域 -->
       <a-col>
         <div class="user-login-status">
-          <a-button type="primary">登录</a-button>
+          <div v-if="loginUserStore.loginUser.id">
+            <a-dropdown>
+              <a-space>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+                {{ loginUserStore.loginUser.userName ?? '未登录的靓仔' }}
+              </a-space>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
+          </div>
+          <div v-else>
+            <a-button type="primary" href="/user/login">登录</a-button>
+          </div>
         </div>
       </a-col>
     </a-row>
@@ -32,7 +50,28 @@
 <script setup lang="ts">
 import { h, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import type { MenuProps } from 'ant-design-vue'
+import { type MenuProps, message } from 'ant-design-vue'
+import { useLoginUserStore } from '@/stores/loginUser.ts'
+import { LogoutOutlined } from '@ant-design/icons-vue'
+import { userLogout } from '@/api/userController.ts'
+
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogout()
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
+}
+
+
+// 获取登录用户状态
+const loginUserStore = useLoginUserStore()
 
 const router = useRouter()
 // 当前选中菜单
